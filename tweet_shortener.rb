f63b8a21581ed23f16e731bfe4f45c612d286834
Matthew Shin <mshin1995@gmail.com>
2019-02-14 10:17:34 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -25,7 +25,9 @@ def word_substituter(tweet)
 end
 
 def bulk_tweet_shortener(tweet)
-  tweet.collect
+  tweet.collect do |tweet|
+    puts word_substituter(tweet)
+  end
 end
 
 def selective_tweet_shortener
